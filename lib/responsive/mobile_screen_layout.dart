0da14e9779a0c7b9ec20a5fad20a53803f7e34b3
Lydia Yang <lydiayang92@gmail.com>
2022-02-07 12:48:06 -0800

    firebase setup

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MobileScreenLayout extends StatelessWidget {
-  const MobileScreenLayout({ Key? key }) : super(key: key);
+  const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
