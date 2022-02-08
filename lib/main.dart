5bb27499a88e80a85f111d8f49b5509bbb8718a0
Lydia Yang <lydiayang92@gmail.com>
2022-02-07 23:46:27 -0800

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_screen_layout.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyAPZ0tVGcxBO1l9eKPTwfITu-2CW4YcAts",
+            appId: "1:222195027251:web:a33e275fc589a37f2e0246",
+            messagingSenderId: "222195027251",
+            projectId: "instagram-clone-683b0",
+            storageBucket: "instagram-clone-683b0.appspot.com"));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -16,10 +32,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Instagram Clone',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: const ResponsiveScreenLayout(mobileScreenLayout: MobileScreenLayout() , webScreenLayout: WebScreenLayout(),),
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      home: LoginScreen(),
+      // home: const ResponsiveScreenLayout(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ),
     );
   }
 }
-
-
