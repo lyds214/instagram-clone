9537e04ed52ca6e54246a49a5e4a64f4e0f9b821
Lydia Yang <lydiayang92@gmail.com>
2022-02-09 00:23:35 -0800

    signup screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/login_screen.dart';
+import 'package:instagram_clone/screens/signup_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 void main() async {
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: const LoginScreen(),
+      home: const SignupScreen(),
       // home: const ResponsiveScreenLayout(
       //   mobileScreenLayout: MobileScreenLayout(),
       //   webScreenLayout: WebScreenLayout(),
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/test_field_input.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Flexible(child: Container(), flex: 2),
+          SvgPicture.asset('assets/ic_instagram.svg',
+              color: primaryColor, height: 64),
+          const SizedBox(
+            height: 64,
+          ),
+          Stack(
+            children: [
+              const CircleAvatar(
+                radius: 64,
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1644325781920-a41ee5b914b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'),
+              ),
+              Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.add_a_photo)))
+            ],
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+              textEditingController: _usernameController,
+              hintText: 'Enter your username',
+              textInputType: TextInputType.text),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+              textEditingController: _emailController,
+              hintText: 'Enter your email',
+              textInputType: TextInputType.emailAddress),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+              textEditingController: _passwordController,
+              hintText: 'Enter your password',
+              isPass: true,
+              textInputType: TextInputType.text),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+              textEditingController: _bioController,
+              hintText: 'Enter your bio',
+              textInputType: TextInputType.text),
+          const SizedBox(
+            height: 24,
+          ),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              child: const Text("Log in"),
+              width: double.infinity,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  color: blueColor),
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Flexible(child: Container(), flex: 2),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  child: const Text("Dont have an account?"),
+                  padding: const EdgeInsets.symmetric(vertical: 8)),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                    child: const Text(
+                      "Sign up.",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 8)),
+              ),
+            ],
+          )
+        ]),
+      )),
+    );
+  }
+}
